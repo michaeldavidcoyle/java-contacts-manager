132e7a5fc07723eddbe4316c93176d6a23676960
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-19 23:16:26 -0500

    Added array of menu items and displayMenu method

--- src/contacts/Main.java
+++ src/contacts/Main.java
@@ -1,4 +1,22 @@
 package contacts;
 
 public class Main {
+
+    private static String[] menu = {
+            "1. View contacts.",
+            "2. Add a new contact.",
+            "3. Search a contact by name.",
+            "4. Delete an existing contact.",
+            "5. Exit."
+    };
+
+    public static void displayMenu() {
+        System.out.println("\n----- Main Menu -----");
+
+        for (String item : menu) {
+            System.out.println(item);
+        }
+
+        System.out.print("Enter an option (1, 2, 3, 4 or 5): ");
+    }
 }
