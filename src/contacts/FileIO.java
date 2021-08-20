d7075a441b5cb11a9acc6365f5e9ec55b5e7ccca
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-19 22:20:55 -0500

    Created fields and method to create and retrieve contacts file

--- src/contacts/FileIO.java
+++ src/contacts/FileIO.java
@@ -1,4 +1,34 @@
 package contacts;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileIO {
+    protected static String directory = "data";
+    protected static String filename = "contacts.txt";
+    protected static Path dataDirectory = Paths.get(directory);
+    protected static Path contactsFile = Paths.get(directory, filename);
+
+    public static Path getContactsFile() throws IOException {
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (Files.notExists(contactsFile)) {
+            Files.createFile(contactsFile);
+        }
+
+        return contactsFile;
+    }
+
+    public static void main(String[] args) {
+        try {
+            getContactsFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
