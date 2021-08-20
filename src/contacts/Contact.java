d019ee355f89b7865111359e6ab5724fcb9ab6d6
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-19 23:12:30 -0500

    Created addContacts method

--- src/contacts/Contact.java
+++ src/contacts/Contact.java
@@ -43,4 +43,24 @@ public class Contact {
     public static List<Contact> getContacts() {
         return contacts;
     }
+
+    public static void addContacts() {
+        Input input = new Input();
+
+        boolean wantsToContinue = true;
+
+        System.out.println("\n----- Add New Contacts -----");
+
+        do {
+            String firstName = input.getString("Enter first name: ");
+            String lastName = input.getString("Enter last name: ");
+            String phoneNumber = input.getString("Enter phone number: ");
+
+            Contact contact = new Contact(firstName, lastName, phoneNumber);
+
+            contacts.add(contact);
+
+            wantsToContinue = input.yesNo("Add another contact? [y/n] ");
+        } while (wantsToContinue);
+    }
 }
